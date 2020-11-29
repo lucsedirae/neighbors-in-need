c239a570720cdcbe96fc1b9d25fa52269f215edb
Jon Deavers <jondeavers@gmail.com>
2020-11-29 11:58:42 -0500

    In process- adding map script to homescreen.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,12 +20,12 @@ module.exports = function(app) {
   app.post("/api/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
-      password: req.body.password,
+      password: req.body.password
     })
       .then(() => {
         res.redirect(307, "/api/login");
       })
-      .catch((err) => {
+      .catch(err => {
         res.status(401).json(err);
       });
   });
@@ -46,17 +46,13 @@ module.exports = function(app) {
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
-        id: req.user.id,
+        id: req.user.id
       });
     }
   });
 
-  app.get("/api/event_locations", (req, res) => {
-    db.EventLocation.findAll({
-      where: {
-        id: req
-      }
-    }).then((eventLocation) => {
+  app.get("/api/eventlocations", (req, res) => {
+    db.eventlocations.findAll({}).then(eventLocation => {
       // We have access to the todos as an argument inside of the callback function
       res.json(eventLocation);
     });
