5bededcda8bd8b973a9c689598b41bead659258a
Mark Major <mmajor@valcv.org>
2020-12-01 21:32:49 -0500

    seeded db with homeless shelters

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,18 @@
 USE nin_db;
 
-INSERT INTO eventlocations (location, address, eventDescription, eventTime, latitude, longitude, createdAt, updatedAt) VALUES ('Emergency Shelters','5 E Franklin St Richmond, VA', "Shelter available", "2020-12-15 17:00:00", 37.543490, -77.443500, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO eventlocations (location, address, eventDescription, eventTime, latitude, longitude, createdAt, updatedAt) VALUES ('Home Again','5 E Franklin St Richmond, VA', "Shelter available", "2020-12-15 17:00:00", 37.543490, -77.443500, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO eventlocations (location, address, eventDescription, eventTime, latitude, longitude, createdAt, updatedAt) VALUES ('First Hillard House','3900 Nine Mile RD Richmond, VA', "Shelter available", "2020-12-15 17:00:00", 37.542240, -77.381200, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO eventlocations (location, address, eventDescription, eventTime, latitude, longitude, createdAt, updatedAt) VALUES ('Commonwealth Catholic Charaties','809 Oliver Hill Way Richmond, VA', "Shelter available", "2020-12-15 17:00:00", 37.540300, -77.423600, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO eventlocations (location, address, eventDescription, eventTime, latitude, longitude, createdAt, updatedAt) VALUES ('Safe Haven','2864 Hull ST Richmond, VA', "Shelter available", "2020-12-15 17:00:00", 37.510510, -77.459890, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO eventlocations (location, address, eventDescription, eventTime, latitude, longitude, createdAt, updatedAt) VALUES ('The Healing Place','700 Dinwiddie Avenue Richmond, VA', "Shelter available", "2020-12-15 17:00:00", 37.517552, -77.434320, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+ 
+
+  The Salvation Army Area Command, 2 West Grace street,
+  Virginia Supportive Housing, 8002 Discovery Drive #201
+  Liberation Veteran Services, 1201 Hull street
+  Shepards Way Relief Center
+  Rondal Mcdonald House Charaties of Richmond, 2330 Monument Avenue -->
