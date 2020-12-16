af52e774da585befb30f414b71e4e0be293b0188
Jon Deavers <jondeavers@gmail.com>
2020-12-15 19:27:07 -0500

    Added compression to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
+const compression = require("compression");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 const exphb = require("express-handlebars");
@@ -13,9 +14,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(compression());
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  //* Session secret is a randomly generated string to improve security. 
+  //? Should be replaced at some point with an autoincrementing variable. Perhaps tied to an autoincrement in the SQL db?
+  session({ secret: "L9RQML1kGE", resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
